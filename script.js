7f59f3862d0215cffcd45d941457df765e9bc9b6
Michael <michael76ramirez@gmail.com>
2023-04-07 21:40:38 -0700

    added scoreboard

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-let input = prompt("Rock, Paper, or Scissors?")
+let playerWins = 0;
+let computerWins = 0;
+
+for(let i=0; i<5; i++){
+let input = prompt("Rock, Paper, or Scissors? (Best out of 5)")
 if(input.toLowerCase() == "rock"){
-    input = 0
+    input = 0;
 }
 else if(input.toLowerCase() == "paper"){
-    input = 1
+    input = 1;
 }
 else if(input.toLowerCase() == "scissors"){
-    input = 2
+    input = 2;
 }
-else(console.log("Input a valid object idiot."))
+else{
+    (console.log("Input a valid object idiot."))
+    continute; //go to next iteration of the loop
+}
 
-getComputerChoice(0,1,2)
+getComputerChoice();
+
+
 function getComputerChoice(){
 let number = Math.floor(Math.random() * 3)
 if(number==0){
@@ -23,37 +32,58 @@ else{
     console.log('Computer chooses Scissors');
 }
 
+let result = singleRound(number, input);
+console.log(result);
+}
 
-singleRound(number, input)
+
 function singleRound(number, input){
     if(number==0 && input==1){
-        console.log("You win.");
+        playerWins++;
+        return("You win.");
     }
     else if(number==1 && input==0){
-        console.log("You lose.");
+        computerWins++;
+        return("You lose.");
     }
     else if(number==0 && input==0){
-        console.log("Tie.");
+        return("Tie.");
     }
     else if(number==1 && input==1){
-        console.log("Tie.");
+        return("Tie.");
     }
     else if(number==0 && input==2){
-        console.log("You lose.");
+        computerWins++;
+        return("You lose.");
     }
     else if(number==2 && input==0){
-        console.log("You win.");
+        playerWins++;
+        return("You win.");
     }
     else if(number==2 && input==1){
-        console.log("You lose.");
+        computerWins++;
+        return("You lose.");
     }
     else if(number==1 && input==2){
-        console.log("You win.");
+        playerWins++;
+        return("You win.");
     }
     else if(number==2 && input==2){
-        console.log("Tie.");
+        return("Tie.");
     }
-    else(console.log("Input a valid object idiot."))
+    else "Input a valid object idiot.";
 }
 }
 
+console.log("Player Wins:" + playerWins);
+console.log("Computer Wins:" + computerWins);
+
+if(playerWins > computerWins){
+    console.log("You win the game!");
+}
+else if(playerWins < computerWins){
+    console.log("You lose the game");
+}
+else{
+    console.log("The game is tied.");
+}
