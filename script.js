75081db56818686a2d2a9f87d1af0b258882dd84
Michael <michael76ramirez@gmail.com>
2023-04-07 19:46:50 -0700

    basic game compelete scoreboard not added

--- script.js
+++ script.js
@@ -1,13 +1,59 @@
-getComputerChoice(0,1,2)
-function getComputerChoice(r,p,s){
-let number = Math.floor(Math.random() * 3)
-if(number == 0){
-    console.log("rock")
+let input = prompt("Rock, Paper, or Scissors?")
+if(input.toLowerCase() == "rock"){
+    input = 0
 }
-else if(number == 1){
-    console.log("paper")
+else if(input.toLowerCase() == "paper"){
+    input = 1
+}
+else if(input.toLowerCase() == "scissors"){
+    input = 2
+}
+else(console.log("Input a valid object idiot."))
+
+getComputerChoice(0,1,2)
+function getComputerChoice(){
+let number = Math.floor(Math.random() * 3)
+if(number==0){
+    console.log("Computer chooses Rock");
+}
+else if(number==1){
+    console.log('Computer chooses Paper');
 }
 else{
-    console.log("scissors")
+    console.log('Computer chooses Scissors');
+}
+
+
+singleRound(number, input)
+function singleRound(number, input){
+    if(number==0 && input==1){
+        console.log("You win.");
+    }
+    else if(number==1 && input==0){
+        console.log("You lose.");
+    }
+    else if(number==0 && input==0){
+        console.log("Tie.");
+    }
+    else if(number==1 && input==1){
+        console.log("Tie.");
+    }
+    else if(number==0 && input==2){
+        console.log("You lose.");
+    }
+    else if(number==2 && input==0){
+        console.log("You win.");
+    }
+    else if(number==2 && input==1){
+        console.log("You lose.");
+    }
+    else if(number==1 && input==2){
+        console.log("You win.");
+    }
+    else if(number==2 && input==2){
+        console.log("Tie.");
+    }
+    else(console.log("Input a valid object idiot."))
 }
 }
+
